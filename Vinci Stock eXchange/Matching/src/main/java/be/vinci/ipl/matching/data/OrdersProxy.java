cf68feae2b60e15ff08e6eb7cd1515766d987217
KekManlol <kemanyinghi@outlook.fr>
2023-11-29 14:49:28 +0100

    add first version of Matching

---
+++ Vinci Stock eXchange/Matching/src/main/java/be/vinci/ipl/matching/data/OrdersProxy.java
@@ -0,0 +1,20 @@
+package be.vinci.ipl.matching.data;
+
+import be.vinci.ipl.matching.models.Order;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Repository
+@FeignClient(name = "orders")
+public interface OrdersProxy {
+
+  @GetMapping("/order/open/by-ticket/{ticker}/{side}")
+  Iterable<Order> findOrdersByTicker(@PathVariable String ticker, @PathVariable String side);
+
+  @PatchMapping("/order/{guid}")
+  boolean updateOrderQuantity(@PathVariable String guid, @RequestBody Order order);
+}
---
+++ Vinci Stock eXchange/Matching/src/main/java/be/vinci/ipl/matching/models/OrderType.java
@@ -0,0 +1,5 @@
+package be.vinci.ipl.matching.models;
+
+public enum OrderType {
+    MARKET, LIMIT;
+}
