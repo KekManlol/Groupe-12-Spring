97593f130dd3c9295e56553351972e6114b13adc
QuocAnDg <quocand40@gmail.com>
2023-11-30 21:56:13 +0100

    feat: optimized codes for wallet service

--- Vinci Stock eXchange/Wallet/src/main/java/be/vinci/ipl/wallet/repositories/WalletRepository.java
+++ Vinci Stock eXchange/Wallet/src/main/java/be/vinci/ipl/wallet/repositories/WalletRepository.java
@@ -1,12 +1,11 @@
 package be.vinci.ipl.wallet.repositories;
 
 import be.vinci.ipl.wallet.model.PositionWithUsername;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WalletRepository extends CrudRepository<PositionWithUsername, Long> {
-  List<PositionWithUsername> findByUsername(String username);
+  Iterable<PositionWithUsername> findByUsername(String username);
 
 }
