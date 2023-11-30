26ff92721037fc2ee2a92a4266e293633acaa347
Porridjo <Den.nguyen69@gmail.com>
2023-11-30 19:47:28 +0100

    add: orders import sql

---
+++ Vinci Stock eXchange/Order/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO orders(guid, owner, order_timestamp, ticker, quantity, side, order_type, limit_price, filled) VALUES ('5073c3b8-4ff0-4169-95cc-14ba61a5b4a6', 'testUser', 123456789, 'AAPL', 10, 'BUY', 'LIMIT', 100.0, 0);
+
+INSERT INTO orders(guid, owner, order_timestamp, ticker, quantity, side, order_type, limit_price, filled) VALUES ('4561c3b8-4ff0-7845-95cc-14ba61a5b4a6', 'testUser2', 223456789, 'LNRD', 10, 'BUY', 'LIMIT', 100.0, 0);
+
+INSERT INTO orders(guid, owner, order_timestamp, ticker, quantity, side, order_type, limit_price, filled) VALUES ('7895c3b8-4ff0-1234-95cc-14ba61a5b4a6', 'testUser', 323456789, 'LNRD', 10, 'BUY', 'LIMIT', 100.0, 8);
+
+INSERT INTO orders(guid, owner, order_timestamp, ticker, quantity, side, order_type, limit_price, filled) VALUES ('1475c3b8-4ff0-1324-12cc-52ba61a5b4a6', 'testUser', 323456789, 'LNRD', 10, 'BUY', 'LIMIT', 100.0, 10);
+
+INSERT INTO orders(guid, owner, order_timestamp, ticker, quantity, side, order_type, limit_price, filled) VALUES ('9785c3b8-4ff0-6347-12cc-52ba61a5b4a6', 'testUser', 323456789, 'LNRD', 10, 'SELL', 'LIMIT', 100.0, 2);
