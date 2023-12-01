ddd2a5bd07fa6aa4a9683202b3f23a8b0630908f
Porridjo <Den.nguyen69@gmail.com>
2023-12-01 14:49:58 +0100

    add order dto in execution

--- Vinci Stock eXchange/execution/src/main/java/be/vinci/ipl/execution/models/Order.java
+++ Vinci Stock eXchange/execution/src/main/java/be/vinci/ipl/execution/models/Order.java
@@ -1,35 +1,34 @@
 package be.vinci.ipl.execution.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@ToString
 public class Order {
-    
-    public enum Side {
-        BUY, SELL;
-    }
 
-    public enum Type {
-        MARKET, LIMIT;
-    }
+  private String guid;
 
-    private String guid;
-    
-    private String owner;
 
-    private String timestamp;
+  private String owner;
 
-    private String ticker;
-    private int quantity;
-    private Side side;
-    private Type type;
+  private int timestamp;
 
-    private double limit;
-    private int filled;
+
+  private String ticker;
+
+
+  private int quantity;
+
+
+  private OrderSide side;
+
+  private OrderType type;
+
+  private double limit;
+  private int filled;
 }
